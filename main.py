24ca341f11561899477c7eba482dbeedcc13ba31
Yarkyka <mariam.siradze.1@btu.edu.ge>
2024-02-28 21:26:37 +0400

    Moved traffic directory from server to main

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from threading import Thread, Event
-import socket
-# Do not import something that you do not use
-from scapy.all import sniff, ARP, UDP,IP, Ether
-from datetime import datetime
 import server
-# try to import only the things you need not the whole files/modules
 import sniffer 
+import os
+from config import BASE_DIRECTORY
 
 event = Event()
 
+TRAFFIC_DIRECTORY = os.path.join(BASE_DIRECTORY, "MACS")
+if not os.path.exists(TRAFFIC_DIRECTORY):
+    os.makedirs(TRAFFIC_DIRECTORY)
+    
 if __name__ == "__main__":
-    # t = Thread(target=server.start_server, args=("127.0.0.1","Hello"))
-    # t.start()
-    # Do not leave unneccessary comments
     server = server.Server("127.0.0.1",event)
     server.start()
-    # sniff(prn=packet_handler, store=0, iface="wlo1") 
     print("Server started")
     sniffer.start_sniffer()
