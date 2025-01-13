56a1d2ac0a977b2ca11e4763fe0f884a244aa305
TheActualSanny <dugna1234@gmail.com>
2025-01-13 21:06:37 +0400

    refactor main.py: Refactor  implementation of sniffer in main

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from threading import Thread, Event
+import logging
 import server
-import sniffer
 import os
 from config import LOOPBACK_ADDRESS
 from target_manager import TargetManager
-
-import logging
+from threading import Thread, Event
 from traffic_logger import logger_setup
 
 logger_setup()
@@ -17,5 +15,4 @@ if __name__ == "__main__":
     target_manager_instance = TargetManager()
     server = server.Server(LOOPBACK_ADDRESS, event, target_manager_instance)
     server.start()
-    logger.info("Server started")
-    sniffer.start_sniffer()
+
