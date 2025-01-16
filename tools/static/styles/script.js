a7d4e9f31f292d23b7c96ef8775ce7f19e9a6193
TheActualSanny <dugna1234@gmail.com>
2025-01-17 03:53:44 +0400

    add feature: Add an initial implementation to loading the packets inon the page, will change this for it to run automatically

--- tools/static/styles/script.js
+++ tools/static/styles/script.js
@@ -10,5 +10,32 @@ function auto_fill() {
     .catch(err => console.log(err))
 }
 
+function load_packets() {
+   fetch("/packets", {method : 'GET'})
+   .then(res => {
+    return res.json();
+   }).then(a => {
+    if (a.packets) {
+        arr = a.packets;
+        console.log(arr);
+        mainDiv = document.getElementById('packet-list');
+        for(let i = 0; i < arr.length; i++){
+                packetData = document.createElement('p');
+                packetData.textContent = `Packet SRC IP: ${arr[i].src_ip}\n DST IP: ${arr[i].dst_ip}`;
+                console.log(packetData.textContent);
+                packetInstance = document.createElement('div');
+                packetInstance.setAttribute('id', 'packet-instance');
+                packetInstance.appendChild(packetData);
+                mainDiv.appendChild(packetInstance);
+        }
+    }
+    }
+    )
+}
+
 var btn = document.getElementById("auto-fill");
+var sniffer = document.getElementById('invoke-form');
+var snifferButton = sniffer.querySelector('#begin');
+
+snifferButton.addEventListener("click", load_packets);
 btn.addEventListener("click", auto_fill);
