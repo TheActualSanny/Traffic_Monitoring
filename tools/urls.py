e968ee2c4023fd479b01612f6b0cea9debe6f10c
TheActualSanny <dugna1234@gmail.com>
2025-01-26 01:38:01 +0400

    update module: Update the urls

--- tools/urls.py
+++ tools/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('start/', views.invoke_sniffer, name = 'invoke-sniffer'),
     path('terminate/', views.terminate_sniffer, name = 'terminate-sniffer'),
     path('update/', views.get_networkifc, name = 'interface-name'),
-    path('packets/', views.get_packets, name = 'get-packets')
+    path('packets/', views.get_packets, name = 'get-packets'),
+    path('lookups/', views.lookup_page, name = 'lookups'),
+    path('initiate/', views.initiate_lookups, name = 'initiate-lookups'),
+    path('lookup_records/', views.get_lookups, name = 'get-lookups'),
+    path('loadmacs/', views.get_macs, name = 'get-macs'),
+    path('managemac/', views.manage_target, name = 'select-mac')
 ]
