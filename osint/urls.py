b6f5bb94ddfe7fc5b5e5d8e5425309a1744ed095
TheActualSanny <dugna1234@gmail.com>
2025-02-20 16:08:42 +0400

    fix: delete redundant templates from tools and fix urls

--- osint/urls.py
+++ osint/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('lookups_api.urls')),
-    path('', include('tools.urls'))
+    path('', include('tools.urls')),
+    path('authenticate/', include('user_authentication.urls', namespace = 'authentication'))
 ]
--- tools/urls.py
+++ tools/urls.py
@@ -4,15 +4,11 @@ from django.urls import path
 app_name = 'tools'
 
 urlpatterns = [
-    # path('', views.register, name = 'register'),
-    # path('login/', views.login_view, name = 'login'),
-    # path('logout/', views.logout_view, name = 'logout'),
     path('tools/', views.add_mac, name = 'add-mac'),
     path('remove/', views.remove_target, name = 'remove-mac'),
     path('start/', views.invoke_sniffer, name = 'invoke-sniffer'),
     path('terminate/', views.terminate_sniffer, name = 'terminate-sniffer'),
     path('update/', views.get_networkifc, name = 'interface-name'),
-    path('packets/', views.get_packets, name = 'get-packets'),
     path('lookups/', views.lookup_page, name = 'lookups'),
     path('initiate/', views.initiate_lookups, name = 'initiate-lookups'),
     path('lookup_records/', views.get_lookups, name = 'get-lookups'),
